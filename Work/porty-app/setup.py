b13075fe27f7dc6cbf749509e1ef9cf064dc6886
l-reich <64188912+l-reich@users.noreply.github.com>
2026-07-02 09:31:27 +0200

    finished course

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Luis Reich",
+    author_email="you@example.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
